b58120b897a94f0dc2c58622aaf4d2739d6dc37e
Kaashi T <133036513+KaashiRaju@users.noreply.github.com>
2026-03-18 11:54:56 +0530

    Delete collectx directory

--- collectx/src/main/java/com/example/collectx/repository/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.collectx.repository;
-
-import com.example.collectx.entity.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
